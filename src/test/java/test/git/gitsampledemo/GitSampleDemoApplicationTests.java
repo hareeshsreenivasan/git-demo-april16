87c8fb8cca5aedaa3af50fc455c2868ec96ebea5
Hareesh Sreenivasan <trivandrum50@gmail.com>
2024-04-16 11:03:38 +0530

    First commit for the project

---
+++ src/test/java/test/git/gitsampledemo/GitSampleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.git.gitsampledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
